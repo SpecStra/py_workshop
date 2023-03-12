5dfd85f1f928159a0a4310b8abda9216e2f71073
SirChain <70142814+SpecStra@users.noreply.github.com>
2023-03-12 14:31:26 +0900

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from ymd_prj.sequence.tube_req import download_thum
-from ymd_prj.sequence.tube_mp3 import download_mp3
-
-url = "https://www.youtube.com/watch?v=YGgahso4Zto"
-# 1. thum
-download_thum(url)
-# 2. mp3
-download_mp3(url)
